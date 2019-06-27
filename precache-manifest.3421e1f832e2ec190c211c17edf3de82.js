e318930ddb376f0803d8434dc115f54d0da6c208
kommunistMaou <kommunistmaou@gmail.com>
2019-06-27 19:01:28 +0300

    Updates

---
+++ precache-manifest.3421e1f832e2ec190c211c17edf3de82.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97f4ef522b59298d49380df26adc72c4",
+    "url": "/github.io/react_jun19_dynamic-list-of-todos/index.html"
+  },
+  {
+    "revision": "22cb96062a1825e00d31",
+    "url": "/github.io/react_jun19_dynamic-list-of-todos/static/css/main.cb9e985a.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/github.io/react_jun19_dynamic-list-of-todos/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "22cb96062a1825e00d31",
+    "url": "/github.io/react_jun19_dynamic-list-of-todos/static/js/main.727c1211.chunk.js"
+  },
+  {
+    "revision": "98f697c33698ee06166b",
+    "url": "/github.io/react_jun19_dynamic-list-of-todos/static/js/runtime~main.c2f18159.js"
+  }
+]);
